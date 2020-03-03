5851f7262747bffcfb20d8a4afe2650045e4552b
TheCroe <61759792+TheCroe@users.noreply.github.com>
2020-03-03 23:55:39 +0300

    Add files via upload

---
+++ kullanıcıdan veri alma.rb
@@ -0,0 +1,21 @@
+print "set@croe::> "
+croe = gets.chomp()
+
+if croe.start_with?("use --help")
+    puts "use alperen/alperen"
+    alperen
+elsif croe.start_with?("use alperenandroid/backdoor_apk")
+    ruby `core/alperen/android/android.rb`
+elsif croe.start_with?("show --help")
+    puts "show mo"
+    puts "show jumper"
+7.times { |alperen|
+     puts "#{alperen} set@croe>> "
+}
+else  
+    puts "gçersi"
+
+    def alperen
+        puts "alperen"
+    end
+end
